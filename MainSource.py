c080342e986e8461a2989dd0f0559cf566756aa5
SCESecure <harry050327@gmail.com>
2021-09-04 22:07:40 +0900

    여러 기능 추가
    - 다른 기능과 통합하여 쓸 수 있게 딕셔너리를 변수 형태로 바꾸어 사용
    - 여러 개의 학교가 감지될 경우 경고 메시지를 사용한 후 프로그램 종료 기능 추가

--- MainSource.py
+++ MainSource.py
@@ -4,47 +4,83 @@ print('[S.I(School Information)]')
 print('<본 프로그램은 API 키가 필요합니다. ("https://open.neis.go.kr/portal/mainPage.do"에서 발급 받을 수 있습니다.)>')
 
 key = input('API 키 입력 : ')
+inputstr = input('학교 입력 : ')
+
+url = 'https://open.neis.go.kr/hub/schoolInfo?Type=xml&pIndex=1&pSize=100&' + key + '&' + 'SCHUL_NM=' + inputstr
+
+content = requests.get(url).content
+dict = xmltodict.parse(content)
+jsonString = json.dumps(dict['schoolInfo']['row'], ensure_ascii=False)
+jsonObj = json.loads(jsonString)
+
+if len(jsonObj) > 1 :
+    print('여러개의 학교가 감지되었습니다. 이름을 구체적으로 입력하여 주십시오.')
+
+    exit()
+
+atpt_ofcdc_sc_code = jsonObj['ATPT_OFCDC_SC_CODE']
+atpt_ofcdc_sc_nm = jsonObj['ATPT_OFCDC_SC_NM']
+sd_schul_code = jsonObj['SD_SCHUL_CODE']
+schul_nm = jsonObj['SCHUL_NM']
+eng_schul_nm = jsonObj['ENG_SCHUL_NM']
+schul_knd_sc_nm = jsonObj['SCHUL_KND_SC_NM']
+lctn_sc_nm = jsonObj['LCTN_SC_NM']
+ju_org_nm = jsonObj['JU_ORG_NM']
+fond_sc_nm = jsonObj['FOND_SC_NM']
+org_rdnzc = jsonObj['ORG_RDNZC']
+org_rdnma = jsonObj['ORG_RDNMA']
+org_rdnda = jsonObj['ORG_RDNDA']
+org_telno = jsonObj['ORG_TELNO']
+hmpg_adres = jsonObj['HMPG_ADRES']
+coedu_sc_nm = jsonObj['COEDU_SC_NM']
+org_faxno = jsonObj['ORG_FAXNO']
+hs_sc_nm = jsonObj['HS_SC_NM']
+indst_specl_ccccl_exst_yn = jsonObj['INDST_SPECL_CCCCL_EXST_YN']
+hs_gnrl_busns_sc_nm = jsonObj['HS_GNRL_BUSNS_SC_NM']
+spcly_purps_hs_ord_nm = jsonObj['SPCLY_PURPS_HS_ORD_NM']
+ene_bfe_sehf_sc_nm = jsonObj['ENE_BFE_SEHF_SC_NM']
+dght_sc_nm = jsonObj['DGHT_SC_NM']
+fond_ymd = jsonObj['FOND_YMD']
+foas_memrd = jsonObj['FOAS_MEMRD']
+load_dtm = jsonObj['LOAD_DTM']
+
 print('어떤 기능을 사용하시겠습니까?')
-print('1. 학교기본정보')
-print('2. 급식식단정보')
+print('1. 학교 기본 정보')
+print('2. 급식 식단 정보')
 maininput = input('입력 : ')
 
-if maininput == 1 :
-    inputstr = input('학교 입력 : ')
+if maininput == '1' :
+    print(schul_nm + '의 기본적인 정보는 다음과 같습니다.')
+    print('')
+    print('시도교육청코드 : ' + atpt_ofcdc_sc_code)
+    print('시도교육청명 : ' + atpt_ofcdc_sc_nm)
+    print('표준학교코드 : ' + sd_schul_code)
+    print('학교명 : ' + schul_nm)
+    print('영문학교명 : ' + eng_schul_nm)
+    print('학교종류명 : ' + schul_knd_sc_nm)
+    print('소재지명 : ' + lctn_sc_nm)
+    print('관할조직명 : ' + ju_org_nm)
+    print('설립명 : ' + fond_sc_nm)
+    print('도로명우편번호 : ' + org_rdnzc)
+    print('도로명주소 : ' + org_rdnma)
+    print('도로명상세주소 : ' + org_rdnda)
+    print('전화번호 : ' + org_telno)
+    print('홈페이지주소 : ' + hmpg_adres)
+    print('남녀공학구분명 : ' + coedu_sc_nm)
+    print('팩스번호 : ' + org_faxno)
+    print('고등학교구분명 : ' + str(hs_sc_nm))
+    print('산업체특별학급존재여부 : ' + indst_specl_ccccl_exst_yn)
+    print('고등학교일반실업구분명 : ' + hs_gnrl_busns_sc_nm)
+    print('특수목적고등학교계열명 : ' + str(spcly_purps_hs_ord_nm))
+    print('입시전후기구분명 : ' + ene_bfe_sehf_sc_nm)
+    print('주야구분명 : ' + dght_sc_nm)
+    print('설립일자 : ' + fond_ymd)
+    print('개교기념일 : ' + foas_memrd)
+    print('수정일 : ' + load_dtm)
 
-    url = 'https://open.neis.go.kr/hub/schoolInfo?Type=xml&pIndex=1&pSize=100&' + key + '&' + 'SCHUL_NM=' + inputstr
+    exit()
 
-    content = requests.get(url).content
-    dict = xmltodict.parse(content)
-    jsonString = json.dumps(dict['schoolInfo']['row'], ensure_ascii=False)
-    jsonObj = json.loads(jsonString)
-
-    print('시도교육청코드 : ' + jsonObj['ATPT_OFCDC_SC_CODE'])
-    print('시도교육청명 : ' + jsonObj['ATPT_OFCDC_SC_NM'])
-    print('표준학교코드 : ' + jsonObj['SD_SCHUL_CODE'])
-    print('학교명 : ' + jsonObj['SCHUL_NM'])
-    print('영문학교명 : ' + jsonObj['ENG_SCHUL_NM'])
-    print('학교종류명 : ' + jsonObj['SCHUL_KND_SC_NM'])
-    print('소재지명 : ' + jsonObj['LCTN_SC_NM'])
-    print('관할조직명 : ' + jsonObj['JU_ORG_NM'])
-    print('설립명 : ' + jsonObj['FOND_SC_NM'])
-    print('도로명우편번호 : ' + jsonObj['ORG_RDNZC'])
-    print('도로명주소 : ' + jsonObj['ORG_RDNMA'])
-    print('도로명상세주소 : ' + jsonObj['ORG_RDNDA'])
-    print('전화번호 : ' + jsonObj['ORG_TELNO'])
-    print('홈페이지주소 : ' + jsonObj['HMPG_ADRES'])
-    print('남녀공학구분명 : ' + jsonObj['COEDU_SC_NM'])
-    print('팩스번호 : ' + jsonObj['ORG_FAXNO'])
-    print('고등학교구분명 : ' + str(jsonObj['HS_SC_NM']))
-    print('산업체특별학급존재여부 : ' + jsonObj['INDST_SPECL_CCCCL_EXST_YN'])
-    print('고등학교일반실업구분명 : ' + jsonObj['HS_GNRL_BUSNS_SC_NM'])
-    print('특수목적고등학교계열명 : ' + str(jsonObj['SPCLY_PURPS_HS_ORD_NM']))
-    print('입시전후기구분명 : ' + jsonObj['ENE_BFE_SEHF_SC_NM'])
-    print('주야구분명 : ' + jsonObj['DGHT_SC_NM'])
-    print('설립일자 : ' + jsonObj['FOND_YMD'])
-    print('개교기념일 : ' + jsonObj['FOAS_MEMRD'])
-    print('수정일 : ' + jsonObj['LOAD_DTM'])
-elif maininput == 2 :
+elif maininput == '2' :
     print('')
 else :
     print('주어진 번호를 입력해주세요')
