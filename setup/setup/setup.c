248bb5043df6124910bc5dae663c9c41ed053556
SCESecure <harry050327@gmail.com>
2021-09-13 12:50:05 +0900

    setup 프로그램에서 파일 만들 때 더 이상 txt 파일이 아닌 json 파일을 만드는 코드 추가

--- setup/setup/setup.c
+++ setup/setup/setup.c
@@ -16,8 +16,6 @@ int main() {
 
 	int key = 0;
 
-	FILE *fs = fopen("basicfile.txt", "w");
-
 	printf("[S.I(School Information) Setup]\n\n");
 	printf("본 프로그램은 셋업 파일 역할로 사용되므로 최초 한번 실행 또는 기본 내용을 바꾸고 싶을 때 사용하시길 바랍니다.\n\n");
 
@@ -25,52 +23,49 @@ int main() {
 	printf("API 키 입력 : ");
 	gets(apikey);
 
-	fputs(apikey, fs);
-	fputs("\n", fs);
-
 	printf("\n시도교육청코드 입력 (중복되는 경우만 해당, 아닐시 무시) : ");
 	gets(atptcode);
-
-	fputs(atptcode, fs);
-	fclose(fs);
-
-	printf("\n<시간표 기능 [숫자만 입력]>\n");
-	printf("학년도 입력(ex : 2021학년도) :  ");
-	gets(ay);
-
-	fputs(ay, fs);
-	fclose(fs);
-	
-	printf("학기 입력 : ");
-	gets(sem);
-
-	fputs(sem, fs);
-	fclose(fs);
-
-	printf("학년 입력 : ");
-	gets(grade);
-
-	fputs(grade, fs);
-	fclose(fs);
-
-	printf("반 입력 : ");
-	gets(class_nm);
-
-	fputs(class_nm, fs);
-	fclose(fs);
-
-	FILE* fs2 = fopen("schoolfile.txt", "w");
 
 	printf("\n학교 입력 : ");
 	gets(school_nm);
 
-	fputs(school_nm, fs2);
-	fputs("\n", fs2);
-	fclose(fs2);
+	printf("\n<시간표 기능 [숫자만 입력]>\n");
+	printf("학년도 입력(ex : 2021학년도) :  ");
+	scanf("%d", &ay);
+	
+	printf("학기 입력 : ");
+	scanf("%d", &sem);
+	fseek(stdin, 0, SEEK_SET);
 
-	FILE* fs3 = fopen("isactive.txt", "w");
-	fputs("active", fs3);
-	fclose(fs3);
+	printf("학년 입력 : ");
+	scanf("%d", &grade);
+	fseek(stdin, 0, SEEK_SET);
+
+	printf("반 입력 : ");
+	scanf("%d", &class_nm);
+	fseek(stdin, 0, SEEK_SET);
+
+	FILE* fs1 = fopen("basicfile.json", "w");
+
+	fprintf(fs1, "{\n");
+	fprintf(fs1, "	\"Basic\": {\n");
+	fprintf(fs1, "		\"apikey\": \"%s\",\n", apikey);
+	fprintf(fs1, "		\"atptcode\": \"%s\",\n", atptcode);
+	fprintf(fs1, "		\"school_nm\": \"%s\",\n", school_nm);
+	fprintf(fs1, "		\"SC\": {\n");
+	fprintf(fs1, "			\"ay\": \"%d\",\n", ay);
+	fprintf(fs1, "			\"sem\": \"%d\",\n", sem);
+	fprintf(fs1, "			\"grade\": \"%d\",\n", grade);
+	fprintf(fs1, "			\"class_nm\": \"%d\"\n", class_nm);
+	fprintf(fs1, "		}\n");
+	fprintf(fs1, "	}\n");
+	fprintf(fs1, "}");
+
+	fclose(fs1);
+
+	FILE* fs2 = fopen("isactive.json", "w");
+	fputs("active", fs2);
+	fclose(fs2);
 
 	printf("\n셋업 과정을 완료했습니다. 이제 이 프로그램은 종료하셔도 좋습니다. (아무 키 입력)\n");
 
